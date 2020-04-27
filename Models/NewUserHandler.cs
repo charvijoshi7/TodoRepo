9377a8266a9ceac501d4b578e96aa02f75a28676
charvijoshi7 <charvijoshi7@gmail.com>
2020-04-27 19:43:48 +0530

    Command Pattern Commit1

---
+++ Models/NewUserHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace TodoApi.Models
+{
+    public class NewUserHandler : IRequestHandler<NewUser, bool>
+    {
+       
+        public Task<bool> Handle(NewUser request, CancellationToken cancellationToken)
+        {
+            // save to database  
+            return Task.FromResult(true);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/TodoItemHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+namespace TodoApi.Models
+
+    //handler for post
+{
+    public class TodoItemHandler : IRequestHandler<TodoItem, bool>
+    {
+        public async Task<bool> Handle(TodoItem request, CancellationToken cancellationToken)
+        {
+            // save to database
+            Console.WriteLine("Saved todoItem to database");
+            return await Task.FromResult(true);
+            throw new NotImplementedException();
+        }
+    }
+}
